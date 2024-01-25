f81a04e3638d335953468b14edd3727e0f2f2aa9
Kyle Florence <kyle.florence@gmail.com>
2024-01-24 18:35:31 -0600

    Conditionally load third party scripts
    In an attempt to speed up functional tests, only load analytics and
    feedback scripts outside of continuous integration.

--- src/components/infoDialog.js
+++
@@ -1,6 +0,0 @@
-const dialog = document.getElementById('dialog')
-document.getElementById('info').addEventListener('click', () => {
-  if (!dialog.open) {
-    dialog.showModal()
-  }
-})
--- src/info.js
+++ src/info.js
@@ -1,14 +1,14 @@
-import { Puzzle } from './puzzle'
-
 const container = document.getElementById('feedback-container')
+const dialog = document.getElementById('dialog')
 const help = document.getElementById('help')
+
+document.getElementById('info').addEventListener('click', () => {
+  if (!dialog.open) {
+    dialog.showModal()
+  }
+})
 
 document.getElementById('feedback').addEventListener('click', () => {
   help.setAttribute('open', 'true')
   container.scrollIntoView(true)
-})
-
-const doorbellOptions = window.doorbellOptions
-document.addEventListener(Puzzle.Events.Updated, (event) => {
-  doorbellOptions.properties.puzzleId = event.detail.state.getId()
 })
