6367a908fee01b984b8f753eb62a485251dba543
Kyle Florence <kyle.florence@gmail.com>
2025-04-02 15:02:25 -0700

    Update puzzle 007

--- test/puzzles/007.js
+++ test/puzzles/007.js
@@ -9,18 +9,18 @@ describe('Puzzle 007', function () {
   before(puzzle.before)
 
   it('should be solved', async function () {
-    await puzzle.clickTile(2, 2)
+    await puzzle.clickTile(1, -1)
     await puzzle.clickModifier('move')
+    await puzzle.clickTile(0, -2)
+
     await puzzle.clickTile(1, 0)
-
-    await puzzle.clickTile(2, 3)
     await puzzle.clickModifier('move')
-    await puzzle.clickTile(1, 4)
+    await puzzle.clickTile(0, 2)
 
-    await puzzle.clickTile(1, 2)
+    await puzzle.clickTile(0, 0)
     await puzzle.clickModifier('rotate')
     await puzzle.clickModifier('move')
-    await puzzle.clickTile(1, 3)
+    await puzzle.clickTile(0, 1)
 
     assert(await puzzle.isSolved())
   })
