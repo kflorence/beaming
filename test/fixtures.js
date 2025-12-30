598dea1db405e6957d6b487ea5923cf58919155b
Kyle Florence <kyle.florence@gmail.com>
2025-12-29 19:40:44 -0800

    Fix --headless

--- test/fixtures.js
+++ test/fixtures.js
@@ -34,7 +34,7 @@ export class PuzzleFixture {
       '--disable-extensions',
       '--disable-gpu',
       // Comment this out to watch the tests run in-browser
-      // '--headless',
+      '--headless',
       '--ignore-certificate-errors',
       '--no-sandbox',
       '--window-size=768,1024'
