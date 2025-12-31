kflorence/beaming

commit cbcc806ad3eee7aa5535c0e6dc74c08707b1e8b4

Implement a level. Fixes #43 (#80)
A level will basically be a puzzle that incorporates other puzzles
within it. Each referenced puzzle will have an anchor point in the main
puzzle. The referenced puzzles can be conditionally applied in a number
of ways:
- included or excluded based on whether they have been solved by the
user
- tiles included or excluded based on the way the user solved the puzzle

Those are the only conditions necessary for MVP, though the
configuration should probably support more flexible options in the
future (including items, for example).

The completion of this PR should include a simple level that illustrates
that all the functionality works. The actual first level that will be
shipped with the MVP of the game will be developed in a separate PR.

src/components/items/modifier.js
@@ -1,0 +1,96 @@
+import { Modifier } from '../modifier.js'
+import { movable } from '../modifiers/move.js'
+import { emitEvent, merge } from '../util.js'
+import { Item } from '../item.js'
+import { Modifiers } from '../modifiers.js'
+import { PuzzleModifier } from '../modifiers/puzzle.js'
+
+export class ModifierItem extends movable(Item) {
+  color
+  item
+  name
+
+  constructor (tile, state) {
+    super(...arguments)
+
+    this.color = state.color ?? ModifierItem.DefaultColor
+    this.name = state.name
+
+    const data = { collidable: true }
+    const style = { fillColor: this.color }
+
+    // Get a throwaway instance of the modifier so we can get the correct icon for it
+    const icon = Modifiers.factory(null, state.modifier).getIcon()
+
+    // Not using .place() here because SymbolItem doesn't allow updates and doesn't work with collisions
+    // For some reason the imported SVGs have a Shape along with a Path item. We only need the Path item.
+    this.item = icon.symbol.getItem().children[1].clone({ insert: false })
+    this.item.set({
+      data,
+      position: tile.center,
+      style,
+      strokeScaling: false
+    })
+
+    this.group.addChild(this.item)
+  }
+
+  onCollision ({ collisionStep, nextStep }) {
+    if (this.color === ModifierItem.DefaultColor || nextStep.color === this.color) {
+      // Collided with an icon of matching color, or the default wildcard color
+      return nextStep.copy({
+        onAdd: () => {
+          nextStep.onAdd()
+
+          this.group.removeChildren()
+          this.parent.removeItem(this)
+
+          const state = this.getState()
+
+          // The modifier will attach to the tile if the item is sticky or if the tile has the StickyItems modifier
+          const parent = (ModifierItem.Sticky.includes(state.modifier.type) ||
+            this.parent.modifiers.some((modifier) => modifier.type === Modifier.Types.StickyItems))
+            ? this.parent
+            : null
+
+          const modifier = Modifiers.factory(parent, state.modifier)
+          const message = modifier.getMessage(this)
+
+          emitEvent(ModifierItem.Events.AddModifier, { item: this, message, modifier })
+        },
+        onRemove: () => {
+          nextStep.onRemove()
+          this.parent.addItem(this)
+          this.group.addChild(this.item)
+        }
+      })
+    }
+
+    return collisionStep
+  }
+
+  static DefaultColor = '#333'
+
+  static Events = Object.freeze({
+    AddModifier: 'modifier-item-add'
+  })
+
+  static schema = () => Object.freeze(merge([
+    Item.schema(Item.Types.Modifier),
+    movable.schema(),
+    {
+      properties: {
+        modifier: {
+          oneOf: [
+            PuzzleModifier.schema()
+          ],
+          type: 'object'
+        }
+      },
+      required: ['modifier']
+    }
+  ]))
+
+  // These modifiers will always stick to the tile
+  static Sticky = Object.freeze([Modifier.Types.Puzzle])
+}

commit 5b3a21fc0ce97ce2dd8d60abdd190aac133c1ea6

Fix some bugs introduced in #80 (#84)
Fixed a bug related to modifier schemas syntax. Fixed bug related to the
modifier item when adding modifiers to tiles. Fixed a bug related to
attaching/detaching modifiers from the DOM. Also added a functional test
for some basic editor features. Should add more editor tests at some
point later.

src/components/items/modifier.js
@@ -1,6 +1,6 @@
 import { Modifier } from '../modifier.js'
 import { movable } from '../modifiers/move.js'
-import { emitEvent, merge } from '../util.js'
+import { merge } from '../util.js'
 import { Item } from '../item.js'
 import { Modifiers } from '../modifiers.js'
 import { PuzzleModifier } from '../modifiers/puzzle.js'
@@ -35,45 +35,32 @@ export class ModifierItem extends movable(Item) {
     this.group.addChild(this.item)
   }
 
-  onCollision ({ collisionStep, nextStep }) {
+  onCollision ({ collisionStep, nextStep, puzzle }) {
     if (this.color === ModifierItem.DefaultColor || nextStep.color === this.color) {
       // Collided with an icon of matching color, or the default wildcard color
-      return nextStep.copy({
-        onAdd: () => {
-          nextStep.onAdd()
+      this.group.removeChildren()
+      this.parent.removeItem(this)
 
-          this.group.removeChildren()
-          this.parent.removeItem(this)
+      const state = this.getState()
 
-          const state = this.getState()
+      // The modifier will attach to the tile if the item is sticky or if the tile has the StickyItems modifier
+      const parent = (ModifierItem.Sticky.includes(state.modifier.type) ||
+        this.parent.modifiers.some((modifier) => modifier.type === Modifier.Types.StickyItems))
+        ? this.parent
+        : null
 
-          // The modifier will attach to the tile if the item is sticky or if the tile has the StickyItems modifier
-          const parent = (ModifierItem.Sticky.includes(state.modifier.type) ||
-            this.parent.modifiers.some((modifier) => modifier.type === Modifier.Types.StickyItems))
-            ? this.parent
-            : null
+      const modifier = Modifiers.factory(parent, state.modifier)
 
-          const modifier = Modifiers.factory(parent, state.modifier)
-          const message = modifier.getMessage(this)
+      puzzle.addModifier(modifier)
+      puzzle.setMessage(modifier.getMessage(this))
 
-          emitEvent(ModifierItem.Events.AddModifier, { item: this, message, modifier })
-        },
-        onRemove: () => {
-          nextStep.onRemove()
-          this.parent.addItem(this)
-          this.group.addChild(this.item)
-        }
-      })
+      return nextStep
     }
 
     return collisionStep
   }
 
   static DefaultColor = '#333'
-
-  static Events = Object.freeze({
-    AddModifier: 'modifier-item-add'
-  })
 
   static schema = () => Object.freeze(merge([
     Item.schema(Item.Types.Modifier),

src/components/layout.js
@@ -181,6 +181,13 @@ export class Layout extends Stateful {
     View.update()
   }
 
+  addModifier (modifier) {
+    const index = this.modifiers[this.modifiers.length - 1]?.index ?? 0
+    modifier.index = index + 1
+    this.modifiers.push(modifier)
+    return this.modifiers
+  }
+
   addTile (offset, state = {}) {
     this.removeTile(offset)
 

src/components/modifier.js
@@ -100,15 +100,8 @@ export class Modifier extends Stateful {
    * Remove listeners and the modifier from the DOM.
    */
   detach () {
-    if (!this.#container) {
-      return
-    }
-
+    this.#container?.remove()
     this.#eventListener.remove()
-    this.#container.remove()
-
-    this.element = undefined
-    this.#container = undefined
   }
 
   dispatchEvent (event, detail) {

src/components/modifiers/move.js
@@ -121,14 +121,14 @@ export const movable = (SuperClass) => class MovableItem extends SuperClass {
   }
 
   onMove () {}
+}
 
-  static schema () {
-    return {
-      properties: {
-        movable: {
-          default: true,
-          type: 'boolean'
-        }
+movable.schema = function schema () {
+  return {
+    properties: {
+      movable: {
+        default: true,
+        type: 'boolean'
       }
     }
   }

src/components/modifiers/puzzle.js
@@ -24,21 +24,31 @@ export class PuzzleModifier extends Modifier {
     puzzle.centerOnImport(state.puzzleId)
 
     // Slight pause so the user can see the tile being centered on
+    // TODO https://github.com/kflorence/beaming/issues/85
     setTimeout(() => {
       puzzle.select(state.puzzleId)
       puzzle.centerOnTile(0, 0)
     }, 250)
   }
 
-  static schema = () => Object.freeze(merge([
-    Modifier.schema(Modifier.Types.Puzzle),
-    {
-      properties: {
-        puzzleId: {
-          type: 'string'
-        }
-      },
-      required: ['puzzleId']
-    }
-  ]))
+  static schema () {
+    const currentId = State.getId()
+    const ids = State.getIds().filter((id) => id !== currentId)
+    const titles = ids.map((id) => State.fromCache(id)?.getTitle() ?? id)
+    return Object.freeze(merge([
+      Modifier.schema(Modifier.Types.Puzzle),
+      {
+        properties: {
+          puzzleId: {
+            enum: ids,
+            options: {
+              enum_titles: titles
+            },
+            type: 'string'
+          }
+        },
+        required: ['puzzleId']
+      }
+    ]))
+  }
 }

src/components/modifiers/rotate.js
@@ -115,24 +115,24 @@ export const rotatable = (SuperClass) => class RotatableItem extends SuperClass 
     this.updateState((state) => { state.rotation = this.rotation })
     this.rotateGroup(rotation)
   }
+}
 
-  static schema () {
-    return {
-      properties: {
-        direction: {
-          type: 'number'
-        },
-        rotatable: {
-          default: true,
-          type: 'boolean'
-        },
-        rotation: {
-          type: 'number'
-        },
-        rotationDegrees: {
-          enum: [30, 60, 90],
-          type: 'number'
-        }
+rotatable.schema = function schema () {
+  return {
+    properties: {
+      direction: {
+        type: 'number'
+      },
+      rotatable: {
+        default: true,
+        type: 'boolean'
+      },
+      rotation: {
+        type: 'number'
+      },
+      rotationDegrees: {
+        enum: [30, 60, 90],
+        type: 'number'
       }
     }
   }

src/components/modifiers/toggle.js
@@ -67,17 +67,17 @@ export const toggleable = (SuperClass) => class ToggleableItem extends SuperClas
     this.toggled = toggled
     this.onToggle()
   }
+}
 
-  static schema () {
-    return {
-      properties: {
-        toggleable: {
-          default: true,
-          type: 'boolean'
-        },
-        toggled: {
-          type: 'boolean'
-        }
+toggleable.schema = function schema () {
+  return {
+    properties: {
+      toggleable: {
+        default: true,
+        type: 'boolean'
+      },
+      toggled: {
+        type: 'boolean'
       }
     }
   }

src/components/puzzle.js
@@ -29,7 +29,6 @@ import { Interact } from './interact'
 import { View } from './view'
 import { Schema } from './schema'
 import { Game } from './game'
-import { ModifierItem } from './items/modifier.js'
 import { Icons } from './icon.js'
 import Tippy from 'tippy.js'
 
@@ -73,6 +72,7 @@ export class Puzzle {
   layers = {}
   layout
   message
+  modifiers = []
   selectedTile
   solved = false
   state
@@ -114,13 +114,31 @@ export class Puzzle {
       { type: 'keyup', handler: this.#onKeyup },
       { type: Modifier.Events.Invoked, handler: this.#onModifierInvoked },
       { type: Modifier.Events.Toggled, handler: this.#onModifierToggled },
-      { type: ModifierItem.Events.AddModifier, handler: this.#onAddModifier },
       { type: 'pointermove', element: elements.canvas, handler: this.#onPointerMove },
       { type: Puzzle.Events.Mask, handler: this.#onMask },
       { type: 'resize', element: window, handler: debounce(this.resize.bind(this)) },
       { type: Stateful.Events.Update, handler: this.#onStateUpdate },
       { type: 'tap', element: elements.canvas, handler: this.#onTap }
     ])
+  }
+
+  addModifier (modifier) {
+    console.log('addModifier', modifier)
+    if (modifier.parent) {
+      modifier.parent.addModifier(modifier)
+      this.updateSelectedTile(modifier.parent)
+    } else {
+      this.layout.addModifier(modifier)
+      this.#updateModifiers()
+    }
+
+    // Update state but don't add a move, since it was the previous move that caused the collision.
+    this.updateState()
+  }
+
+  centerOn (r, c) {
+    const point = this.layout.getPoint(new OffsetCoordinates(r, c))
+    View.setCenter(point)
   }
 
   centerOnImport (id) {
@@ -311,6 +329,18 @@ export class Puzzle {
     emitEvent(Puzzle.Events.Updated, { state: this.state })
   }
 
+  removeModifier (modifier) {
+    if (modifier.parent) {
+      modifier.parent.removeModifier(modifier)
+    } else {
+      this.layout.removeModifier(modifier)
+      this.#updateModifiers()
+    }
+
+    // Update state but don't add a move, since it was the previous move that caused the collision.
+    this.updateState()
+  }
+
   resize (reload = true, event = true) {
     const { width, height } = elements.wrapper.getBoundingClientRect()
     const newSize = new Size(width, height)
@@ -355,6 +385,10 @@ export class Puzzle {
 
     // Can't remove puzzles that exist in configuration
     elements.delete.classList.toggle('hide', Puzzles.has(id))
+  }
+
+  setMessage (message) {
+    elements.headerMessage.textContent = message
   }
 
   tap (event) {
@@ -426,7 +460,7 @@ export class Puzzle {
     this.selectedTile = tile
     this.state.setSelectedTile(tile)
     this.#updateMessage(tile)
-    this.#updateModifiers(tile, previouslySelectedTile)
+    this.#updateModifiers()
 
     if (previouslySelectedTile && previouslySelectedTile !== tile) {
       previouslySelectedTile.onDeselected(tile)
@@ -459,34 +493,13 @@ export class Puzzle {
     // .sort((a, b) => a.id - b.id)
   }
 
-  #onAddModifier (event) {
-    const modifier = event.detail.modifier
-
-    if (modifier.parent) {
-      modifier.parent.addModifier(modifier)
-      this.updateSelectedTile(modifier.parent)
-    } else {
-      const index = this.layout.modifiers[this.layout.modifiers.length - 1]?.index ?? 0
-      modifier.index = index + 1
-      this.layout.modifiers.push(modifier)
-      this.#updateModifiers(this.selectedTile)
-    }
-
-    // Update state but don't add a move, since it was the previous move that caused the collision.
-    this.updateState()
-
-    const message = event.detail.message
-    if (message) {
-      elements.headerMessage.textContent = message
-    }
-  }
-
   #onBack () {
     const id = this.state.getId()
     const parentId = State.getParent(id)
     this.centerOnTile(0, 0)
 
     // Slight pause so the user can see the tile being centered on
+    // TODO https://github.com/kflorence/beaming/issues/85
     setTimeout(() => {
       this.select(parentId)
       this.centerOnImport(id)
@@ -550,7 +563,7 @@ export class Puzzle {
       modifier.move(tile)
       console.debug('Modifier is stuck to tile', modifier, tile)
       if (!selectedTile) {
-        this.#updateModifiers(this.selectedTile)
+        this.#updateModifiers()
       }
     }
 
@@ -837,13 +850,12 @@ export class Puzzle {
     elements.headerMessage.textContent = this.message
   }
 
-  #updateModifiers (tile, previousTile) {
-    const modifiers = this.#getModifiers(tile)
-    const previousModifiers = (previousTile ? this.#getModifiers(previousTile) : modifiers)
-    previousModifiers.forEach((modifier) => modifier.detach())
-    modifiers.forEach((modifier) => modifier.attach(tile))
+  #updateModifiers () {
+    this.modifiers.forEach((modifier) => modifier.detach())
+    this.modifiers = this.#getModifiers(this.selectedTile)
+    this.modifiers.forEach((modifier) => modifier.attach(this.selectedTile))
 
-    elements.footer.classList.toggle(Puzzle.ClassNames.Active, modifiers.length > 0)
+    elements.footer.classList.toggle(Puzzle.ClassNames.Active, this.modifiers.length > 0)
   }
 
   // Filters for all beams that are connected to the terminus, or have been merged into a beam that is connected

src/styles.css
@@ -600,10 +600,6 @@ dialog ul {
   border-right: none;
 }
 
-#puzzle-header-message {
-  background-color: white;
-}
-
 #select {
   appearance: none;
   border: none;

test/editor/config.js
@@ -1,0 +1,40 @@
+/* eslint-env mocha */
+import { PuzzleFixture, removeKeys } from '../fixtures.js'
+import assert from 'assert'
+
+describe('Editor', function () {
+  const puzzle = new PuzzleFixture('config', 'edit')
+
+  after(puzzle.after)
+  before(puzzle.before)
+
+  it('should produce matching configuration', async function () {
+    await puzzle.clickAtOffset(0, 0)
+    await puzzle.clickElement('#editor-lock')
+    await puzzle.clickAtOffset(0, 0)
+    await puzzle.clickElement('div[data-schemapath="puzzle.items"] .json-editor-opt-in')
+    await puzzle.clickElement('div[data-schemapath="puzzle.items"] .json-editor-btn-add')
+    await puzzle.selectOption('div[data-schemapath="puzzle.items.0"] .je-switcher', 'portal')
+    await puzzle.clickElement('#editor-update')
+
+    const state = removeKeys(await puzzle.getEditorState(), 'id')
+
+    assert.deepStrictEqual(state, {
+      layout: {
+        offset: 'odd-row',
+        tiles: {
+          0: {
+            0: {
+              type: 'tile',
+              items: [
+                {
+                  type: 'portal'
+                }
+              ]
+            }
+          }
+        }
+      }
+    })
+  })
+})

test/fixtures.js
@@ -7,18 +7,34 @@ logging.installConsoleHandler()
 const logger = logging.getLogger('')
 logger.setLevel(logging.Level.DEBUG)
 
-export class PuzzleFixture {
-  driver
-  elements = {
-    body: undefined,
-    canvas: undefined,
-    modifiers: undefined
+export function removeKeys (obj, keys) {
+  if (!obj || typeof obj !== 'object') {
+    return obj
   }
 
-  constructor (id) {
+  if (!Array.isArray(keys)) {
+    keys = [keys]
+  }
+
+  Object.keys(obj).forEach((key) => {
+    if (keys.includes(key)) {
+      delete obj[key]
+    } else {
+      removeKeys(obj[key], keys)
+    }
+  })
+
+  return obj
+}
+
+export class PuzzleFixture {
+  driver
+  elements = {}
+
+  constructor (id, mode = 'play') {
     this.after = this.after.bind(this)
     this.before = this.before.bind(this)
-    this.url = `${PuzzleFixture.baseUrl}/?play#/${id}`
+    this.url = `${PuzzleFixture.baseUrl}/?${mode}#/${id}`
   }
 
   async after () {
@@ -53,15 +69,16 @@ export class PuzzleFixture {
     this.elements.canvas = await this.driver.findElement(By.id('puzzle'))
   }
 
-  async invokeModifier (name, options = {}) {
-    const times = options.times ?? 1
-    for (let i = 0; i < times; i++) {
-      // Need to re-get modifier each time as location might change
-      const origin = await this.#getModifier(name)
-      const actions = this.driver.actions({ async: true }).move({ origin })
-      actions.press(options.button).release(options.button)
-      await actions.perform()
-    }
+  async clickAtOffset (r, c) {
+    // Center on the point first to ensure it is visible
+    await this.driver.executeScript(`return game.puzzle.centerOn(${r}, ${c})`)
+    return this.driver.actions({ async: true }).move({ origin: this.elements.canvas }).click().perform()
+  }
+
+  async clickElement (selector) {
+    console.log(`Clicking on element: ${selector}`)
+    const origin = await this.getElement(selector)
+    return this.driver.actions({ async: true }).move({ origin }).click().perform()
   }
 
   async clickTile (r, c, onlyIfNotSelected = false) {
@@ -75,12 +92,37 @@ export class PuzzleFixture {
     return Promise.resolve()
   }
 
+  async getEditorState () {
+    const state = await this.driver.executeScript('return game.editor.getState()')
+    console.log('Got editor state:', JSON.stringify(state, null, 2))
+    return state
+  }
+
+  async getElement (selector) {
+    return this.driver.wait(until.elementLocated(By.css(selector)))
+  }
+
+  async getModifier (name) {
+    return this.driver.wait(until.elementLocated(By.css(`.modifier-${name.toLowerCase()}:not(.disabled)`)))
+  }
+
   async isMasked () {
     return this.driver.wait(untilElementHasClass(this.elements.body, 'puzzle-mask'))
   }
 
   async isNotMasked () {
     return this.driver.wait(untilElementDoesNotHaveClass(this.elements.body, 'puzzle-mask'))
+  }
+
+  async invokeModifier (name, options = {}) {
+    const times = options.times ?? 1
+    for (let i = 0; i < times; i++) {
+      // Need to re-get modifier each time as location might change
+      const origin = await this.getModifier(name)
+      const actions = this.driver.actions({ async: true }).move({ origin })
+      actions.press(options.button).release(options.button)
+      await actions.perform()
+    }
   }
 
   async isSolved () {
@@ -121,6 +163,11 @@ export class PuzzleFixture {
     }
   }
 
+  async selectOption (selector, value) {
+    const element = await this.getElement(selector)
+    return await element.sendKeys(value)
+  }
+
   async selectTile (r, c) {
     await this.clickTile(r, c, true)
     return this.driver.wait(untilElementHasClass(this.elements.body, `tile-selected_${r}_${c}`))
@@ -130,10 +177,6 @@ export class PuzzleFixture {
     for (const action of actions) {
       await this.process(action)
     }
-  }
-
-  async #getModifier (name) {
-    return this.driver.wait(until.elementLocated(By.css(`.modifier-${name.toLowerCase()}:not(.disabled)`)))
   }
 
   static baseUrl = 'http://localhost:1234'