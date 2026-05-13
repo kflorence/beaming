79126375f124ab5214b9db2d71ef873e56521ccd
Kyle Florence <kyle.florence@gmail.com>
2026-05-12 19:04:35 -0700

    Fix electron forge ASAR configuration. From #131

--- forge.config.js
+++ forge.config.js
@@ -5,14 +5,15 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses'
 // noinspection JSUnusedGlobalSymbols
 export default {
   packagerConfig: {
-    asar: true,
+    asar: {
+      unpack: '{**/steamworks_sdk/**,**/steamworks-ffi-node/**}'
+    },
     icon: './src/images/icon',
     // See: https://github.com/electron/universal/issues/36
     osxSign: {},
     // See: https://electron.github.io/packager/main/interfaces/OsxUniversalOptions.html
     osxUniversal: {
-      mergeASARs: true,
-      x64ArchFiles: 'Contents/Resources/app.asar.unpacked/node_modules/**/*.node'
+      mergeASARs: true
     }
   },
   rebuildConfig: {},
@@ -37,10 +38,6 @@ export default {
     }
   ],
   plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {}
-    },
     // Fuses are used to enable/disable various Electron functionality
     // at package time, before code signing the application
     new FusesPlugin({
